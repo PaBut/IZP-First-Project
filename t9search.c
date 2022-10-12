ce086be65a1c6073e122bc5f7dc1504b5f559e58
PaBut <xbuten00@stud.fit.vutbr.cz>
2022-10-12 02:01:46 +0200

    Trying to fix the bugs. Precisely it goes after TODO. To do tomorrow

--- t9search.c
+++ t9search.c
@@ -7,6 +7,7 @@ void findContacts(char value[]);
 void getString(char *arr);
 bool checkIfAppropriate(int startPoint, char str[], int startPoint2, char wanted[]);
 void clearString(char str[]);
+char toLow(char c);
 
 char dictionary[10][5] = {
     "+",
@@ -70,7 +71,7 @@ void getString(char arr[])//Get name or number of a contact. Also checking here,
             exit(-1);
         }
         
-        arr[i] = getc(stdin);
+        arr[i] = toLow(getc(stdin));
         
         if(arr[i] == EOF){
             return;
@@ -97,27 +98,37 @@ char toLow(char c){
     }
     return c;
 }
+char toUpper(char c){
+    if(c >= 'a' && c <= 'z'){
+        return c - ('a'-'A');
+    }
+    return c;
+}
 
 
 bool checkIfAppropriate(int startPoint, char str[], int startPoint2, char wanted[]){
-    for(int i = startPoint; i < strlen(str)-1; i++){
+    for(int i = startPoint; i < strlen(str); i++){//TODO:On the last iteration(when i = len - 1), on the next attempt it misses the cycle and returns false 
+
+        //printf("\n%d %c", i, str[i]);
+
         if(startPoint2 == strlen(wanted)){ //base case of recurze
+            //printf("\n\t%s - %c\n", str, str[i]); 
             return true;
         }
 
         bool conditions = false;
-        char ch;
 
         if(str[i]>='0' && str[i] <= '9' ){
             conditions = str[i] == wanted[startPoint2];
-        }else if((ch = toLow(str[i])) >= 'a' && ch <= 'z'){
+        }else if(str[i] >= 'a' && str[i] <= 'z'){
             int index = wanted[startPoint2] - ('0' - '\0');
             if(dictionary[index] != NULL)
             {
                 for(int j = 0; dictionary[index][j] != '\0'; j++)
                 {
-                    if(ch == dictionary[index][j])
+                    if(str[i] == dictionary[index][j])
                     {
+                        //printf("\n\t\t%s - %c\n", str, str[i]);
                         conditions = true;
                         break;
                     }
@@ -127,7 +138,11 @@ bool checkIfAppropriate(int startPoint, char str[], int startPoint2, char wanted
         }
 
         if(conditions){
+            printf("\n%s - %c - %d\n", str, str[i], startPoint2);
             if(checkIfAppropriate(i+1, str, startPoint2+1, wanted)){
+                printf("\n\t\t\t%s - %c - %d\n", str, str[i], startPoint2);
+
+                str[i] = toUpper(str[i]);
                 return true;
             }
         }
